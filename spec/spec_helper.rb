a14c5198d32a50765a212b567d5f3cce461b62ef
Fabio Rehm <fgrehm@gmail.com>
2012-11-07 22:17:18 -0200

    Remove unecessary fileutils require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'tiny-rails'
 require 'tiny-rails/cli'
 
-require 'fileutils'
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
