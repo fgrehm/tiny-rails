044102ee2b4de7f59a176b32788fe745d238072e
Fabio Rehm <fgrehm@gmail.com>
2012-11-10 18:50:33 -0200

    Remove asset pipeline related templates

--- templates/application.coffee
+++
@@ -1,6 +0,0 @@
-#= require jquery
-
-jQuery ($) ->
-  setTimeout ->
-    $('body').html('<h1 id="loaded">Page loaded!</h1>')
-  , 1500
--- templates/application.js
+++
@@ -1,7 +0,0 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require_self
