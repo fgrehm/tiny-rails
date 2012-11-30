c862e7d716d42baa1d96659bbafc51679874baa6
Fabio Rehm <fgrehm@gmail.com>
2012-11-29 22:30:03 -0200

    Move client side validations initializer code from initializers.rb to gem

--- addons/client_side_validations.rb
+++ addons/client_side_validations.rb
@@ -7,13 +7,4 @@ gem 'client_side_validations'
 sentinel = {:after => "#= require jquery\n"}
 inject_into_file 'application.coffee', "#= require rails.validations\n", sentinel
 
-initializer_code = <<-CODE
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  unless html_tag =~ /^<label/
-    %{<div class="field_with_errors">\#{html_tag}<label for="\#{instance.send(:tag_id)}" class="message">\#{instance.error_message.first}</label></div>}.html_safe
-  else
-    %{<div class="field_with_errors">\#{html_tag}</div>}.html_safe
-  end
-end
-CODE
-initializer initializer_code
+initializer 'require "tiny-rails/app/initializers/client_side_validations"'
